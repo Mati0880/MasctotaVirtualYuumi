f4b804472e3ee864ac23152dea758107af33fb7b
Mati0880 <mata.valenzuela@duocuc.cl>
2026-05-22 20:52:14 -0400

    Add files via upload

---
+++ MascotaVirtual.py
@@ -0,0 +1,32 @@
+nombre = "Yummi"
+energia = 100
+felicidad = 100
+print(f"Energía inicial de: {nombre}, energía: {energia}, felicidad: {felicidad}")
+while energia > 0:
+    print("¿Que quieres hacer?:")
+    print("1.- Alimentar")
+    print("2.- Jugar")
+    print("3.- Ver estado de salud")
+    print("4.- No hacer nada")
+    opcion = input("Seleccione: ")
+    if opcion == "1":
+        energia = energia + 20
+        felicidad = felicidad + 1
+        print(f"Alimentaste  a {nombre}, está muy feliz contigo.. ")
+    elif opcion == "2":
+        energia = energia - 15
+        felicidad = felicidad + 20
+        print(f"{nombre} esta cansado pero felíz de jugar contigo")
+    elif opcion == "3":
+        print("    /\__/|   ")
+        print("   ( o  o )  ")
+        print("   /      \  ")
+        print("  (        ) ")
+        print(f"Energia: {energia}")
+        print(f"Felicidad: {felicidad}")
+    elif opcion == "4":
+        felicidad = felicidad - 30
+        energia = energia - 5
+        print(f"{nombre} está muy aburrido....")
+    else:
+        print("Error de ingreso")
